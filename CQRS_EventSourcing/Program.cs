9298303d256345feb271cc603a155cc4a947e53d
Pavel Alexeev <Pavel.Alexeev@quantori.com>
2024-02-27 22:23:26 +0400

    Code cleanup

--- CQRS_EventSourcing/Program.cs
+++ CQRS_EventSourcing/Program.cs
@@ -1,14 +1,4 @@
-﻿using Autofac;
-using CQRS_EventSourcing.DomainModels;
-using CQRS_EventSourcing.EventBus.Interfaces;
-using CQRS_EventSourcing.EventBus.Implementations;
-using CQRS_EventSourcing.EventStore.Interfaces;
-using CQRS_EventSourcing.EventStore.Implementations;
-using CQRS_EventSourcing.Services.QueryService.Interfaces;
-using CQRS_EventSourcing.Services.QueryService.Implementations;
-using CQRS_EventSourcing.Services.CommandService.Interfaces;
-using CQRS_EventSourcing.Services.CommandService.Implementations;
-using CQRS_EventSourcing.Client;
+﻿using CQRS_EventSourcing.Client;
 
 class CQRS_EventSourcingModel
 {
